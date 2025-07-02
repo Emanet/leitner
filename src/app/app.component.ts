b050b95518460819ea9af67bf0bfe88b5e53295c
Mehmetcan <mehmetcanemanet@gmail.com>
2025-07-02 16:39:41 +0200

    fix layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+import { DriverCardComponent } from './components/driver-card/driver-card.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { Driver } from './models/driver.model';
+import { DriverService } from './services/driver.services';
+import { SpeedService } from './services/speed.services';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    FooterComponent,
+    FooterComponent,
+    DriverCardComponent,
+  ],
 })
-export class AppComponent {
-  title = 'leitner';
+export class AppComponent implements OnInit {
+  car1Speed: number = 0;
+  car2Speed: number = 0;
+  drivers: Driver[] = [];
+
+  constructor(
+    private speedService: SpeedService,
+    private driverService: DriverService
+  ) {}
+
+  ngOnInit(): void {
+    this.speedService.car1Speed$.subscribe((speed) => (this.car1Speed = speed));
+    this.speedService.car2Speed$.subscribe((speed) => (this.car2Speed = speed));
+
+    this.driverService
+      .getDrivers()
+      .subscribe((drivers) => (this.drivers = drivers));
+  }
 }
